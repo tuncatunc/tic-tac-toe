f74f6dbf91076f0a10bba6aa6993ce65e4decd2a
Your Name <you@example.com>
2022-03-10 09:47:34 +0300

    play game

--- tests/tic-tac-toe.ts
+++ tests/tic-tac-toe.ts
@@ -27,14 +27,71 @@ describe("tic-tac-toe", () => {
     console.log(gameState);
     expect(gameState.turn).to.equal(1);
     expect(gameState.players).to.eql([playerOne.publicKey, playerTwo.publicKey]);
-    expect(gameState.state).to.eql({active: {}});
+    expect(gameState.state).to.eql({ active: {} });
     expect(gameState.board).to.eql(
       [
         [null, null, null],
         [null, null, null],
         [null, null, null]
       ]
-      )
+    )
     console.log("Your transaction signature", tx);
   });
+
+  it('player one wins', async () => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = program.provider.wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+    await program.rpc.setupGame(playerTwo.publicKey, {
+      accounts: {
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [gameKeypair]
+    });
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players)
+      .to
+      .eql([playerOne.publicKey, playerTwo.publicKey]);
+    expect(gameState.state).to.eql({ active: {} });
+    expect(gameState.board)
+      .to
+      .eql([[null, null, null], [null, null, null], [null, null, null]]);
+
+    await play(
+      program,
+      gameKeypair.publicKey,
+      playerOne,
+      { row: 0, column: 0 },
+      2,
+      { active: {}, },
+      [
+        [{ x: {} }, null, null],
+        [null, null, null],
+        [null, null, null]
+      ]
+    );
+  });
+
 });
+
+async function play(program, game, player,
+  tile, expectedTurn, expectedGameState, expectedBoard) {
+  await program.rpc.play(tile, {
+    accounts: {
+      player: player.publicKey,
+      game
+    },
+    signers: player instanceof (anchor.Wallet as any) ? [] : [player]
+  });
+
+  const gameState = await program.account.game.fetch(game);
+  expect(gameState.turn).to.equal(expectedTurn);
+  expect(gameState.state).to.eql(expectedGameState);
+  expect(gameState.board)
+    .to
+    .eql(expectedBoard);
+}
